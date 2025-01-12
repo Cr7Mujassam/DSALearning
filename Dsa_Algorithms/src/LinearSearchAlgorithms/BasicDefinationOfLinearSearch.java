28e9fcb39bee20c5efee94249cefb2ea241e11a0
Mujassam Taukir Khan <106069072+Cr7Mujassam@users.noreply.github.com>
2025-01-12 20:05:33 +0530

    This is Linear Search Alogithm file

---
+++ Dsa_Algorithms/src/LinearSearchAlgorithms/BasicDefinationOfLinearSearch.java
@@ -0,0 +1,9 @@
+package LinearSearchAlgorithms;
+
+public class BasicDefinationOfLinearSearch {
+
+	public static void main(String[] args) {
+		System.out.println("Hello This is Linear Search");
+	}
+
+}
